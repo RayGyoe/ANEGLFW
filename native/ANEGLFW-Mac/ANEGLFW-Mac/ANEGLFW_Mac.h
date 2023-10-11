843d02cdcf38851974e9eda613e6306e0fd3ee42
ray.lei <ray.lei@edoctor.cn>
2023-10-11 18:48:23 +0800

    test mac

---
+++ native/ANEGLFW-Mac/ANEGLFW-Mac/ANEGLFW_Mac.h
@@ -0,0 +1,18 @@
+//
+//  ANEGLFW_Mac.h
+//  ANEGLFW-Mac
+//
+//  Created by 雷晓卫 on 2023/10/11.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ANEGLFW_Mac.
+FOUNDATION_EXPORT double ANEGLFW_MacVersionNumber;
+
+//! Project version string for ANEGLFW_Mac.
+FOUNDATION_EXPORT const unsigned char ANEGLFW_MacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ANEGLFW_Mac/PublicHeader.h>
+
+
---
+++ native/ANEGLFW-Mac/ANEGLFW-MacTests/ANEGLFW_MacTests.m
@@ -0,0 +1,36 @@
+//
+//  ANEGLFW_MacTests.m
+//  ANEGLFW-MacTests
+//
+//  Created by 雷晓卫 on 2023/10/11.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ANEGLFW_MacTests : XCTestCase
+
+@end
+
+@implementation ANEGLFW_MacTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
