fa093245a6186fe1f0cc63b1ed91e8704a28bcf9
Ray.lei <ray.lei@edoctor.cn>
2025-09-01 18:21:26 +0800

    从远程删除 folder_name 文件夹

--- native/ANEGLFW-Mac/build/ANEGLFW-Mac.build/Debug/ANEGLFW-Mac.build/DerivedSources/ANEGLFW_Mac_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char ANEGLFW_MacVersionString[];
- extern const double ANEGLFW_MacVersionNumber;
-
- const unsigned char ANEGLFW_MacVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:ANEGLFW_Mac  PROJECT:ANEGLFW-Mac-1" "\n";
- const double ANEGLFW_MacVersionNumber __attribute__ ((used)) = (double)1.;
